b2b583b78b942a7b4c777272ed22239990a08737
heverton100 <heverton.rc.100@gmail.com>
2026-08-20 15:05:22 -0300

    Arquitetura MVC - Service, Repository e Controller

---
+++ src/main/java/com/github/heverton100/arquiteturaspring/todos/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.github.heverton100.arquiteturaspring.todos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+
+}
