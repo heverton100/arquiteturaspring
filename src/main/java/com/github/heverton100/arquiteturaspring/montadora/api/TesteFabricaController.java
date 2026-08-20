d9b2c07da197b802c6d7d60cba91f3cd1f5d2e99
heverton100 <heverton.rc.100@gmail.com>
2026-08-20 12:46:17 -0300

    Qualifiers, beans e criando annotations.

--- src/main/java/com/github/heverton100/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ src/main/java/com/github/heverton100/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,6 +2,7 @@ package com.github.heverton100.arquiteturaspring.montadora.api;
 
 import com.github.heverton100.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
+    @Eletrico
     private Motor motor;
 
     @PostMapping
--- src/main/java/com/github/heverton100/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ src/main/java/com/github/heverton100/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -4,18 +4,42 @@ import com.github.heverton100.arquiteturaspring.montadora.Motor;
 import com.github.heverton100.arquiteturaspring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor() {
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado() {
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindros(4);
-        motor.setModelo("ABC-10");
-        motor.setLitragem(2.0);
+        motor.setModelo("AS-50");
+        motor.setLitragem(1.0);
         motor.setTipo(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico() {
+        var motor = new Motor();
+        motor.setCavalos(130);
+        motor.setCilindros(3);
+        motor.setModelo("EL-10");
+        motor.setLitragem(1.6);
+        motor.setTipo(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Primary
+    @Bean(name = "motorTurbo")
+    public Motor motorTurbo() {
+        var motor = new Motor();
+        motor.setCavalos(200);
+        motor.setCilindros(4);
+        motor.setModelo("TR-30");
+        motor.setLitragem(2.0);
+        motor.setTipo(TipoMotor.TURBO);
         return motor;
     }
 
